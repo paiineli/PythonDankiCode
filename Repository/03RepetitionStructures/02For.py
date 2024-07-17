a8fde9b8a77f47d00ec62b9e176fb3494bfb902f
Lucas Paineli <painelilucas@gmail.com>
2024-07-17 19:48:30 -0300

    update repository

--- Repository/03RepetitionStructures/02For.py
+++ Repository/03RepetitionStructures/02For.py
@@ -14,3 +14,17 @@ for x in s:
 # second example
 for x in range(6):
     print(x)
+
+# third example
+for x in range(5, 8):
+    print(x)
+
+# fourth example
+for x in range(0, 6, 2):  # range(6)
+    print(x)
+
+# fifth example
+for x in range(6):  # for(x=0;x<=5;x++)
+    print(x)
+else:
+    print("we've reached the end")
