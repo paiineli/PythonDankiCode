cb7349ea8181c4ca6d9df8ca3e15be8dd260d561
Lucas Paineli <painelilucas@gmail.com>
2024-07-17 15:45:30 -0300

    create association

---
+++ Repository/02ConditionalAndLogicalStructures/05Association.py
@@ -0,0 +1,17 @@
+"""
+association operators
+
+name = "lucas"
+
+in operator - x in y
+not in operator - x not in y
+
+"""
+
+x = "sorocaba"
+y = "soro"
+
+print(y in x)
+print("s" in x)
+print("city" in x)
+print("city" not in x)
