990adae2bfbeea53908c0b5b58b4eac03108768f
Lucas Paineli <painelilucas@gmail.com>
2024-07-17 13:26:47 -0300

    Create 08DataType.py

---
+++ Repository/01VariablesAndDataTypes/08DataType.py
@@ -0,0 +1,20 @@
+"""
+variables and data types
+
+date types:
+
+text types: str
+number types: int, float and complex
+
+sequence types: list, tuple, range
+mapping types: dict
+set types: set, frozenset
+
+boolean type: bool
+binary types: bytes, bytearray, memoryview
+
+print()
+type()
+#casting float, int, str
+
+"""
