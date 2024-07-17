e7aa7f900dfcf22467b3534ea7a137e338f984b2
Lucas Paineli <painelilucas@gmail.com>
2024-07-17 13:22:18 -0300

    Create 07VariableScope.py

---
+++ Repository/01VariablesAndDataTypes/07VariableScope.py
@@ -0,0 +1,23 @@
+# variables scope
+
+# the global variables
+# the local variables
+
+x = 5
+def funcao():
+    x = 3
+    print("local variable value: ",x)
+
+funcao()
+print("global varibale value: ",x)
+
+
+y = 'Lucas' #name
+z = 1.85 #height
+t = "000.000.000-00" #id
+l = 21 #years
+
+name = 'Lucas'
+height = 1.85
+id = "000.000.000-00"
+years = 21
