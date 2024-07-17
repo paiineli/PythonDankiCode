c98541d168a56f39f6a9095afef9e14e74f0d6c4
Lucas Paineli <painelilucas@gmail.com>
2024-07-17 11:44:10 -0300

    Create 06Casting.py

---
+++ Repository/01VariablesAndDataTypes/06Casting.py
@@ -0,0 +1,35 @@
+#int
+
+x = int(2)
+y = int(2.8)
+z = int('2')
+
+print(x, y, z)
+
+#float
+
+a = float(2.3)
+b = float(2)
+c = float('2.3')
+d = float('2')
+
+print(a, b, c, d)
+
+#str
+
+t = 's1'
+r = 2
+z = 2.3
+
+print("the type of variable t is: ", type(t)) #the type of variable t is:  <class 'str'>
+print("the type of variable r is: ", type(r)) #the type of variable r is:  <class 'int'>
+print("the type of variable s is: ", type(z)) #the type of variable s is:  <class 'float'>
+
+t = str('s1')
+r = str(2)
+z = str(2.3)
+
+print("the type of variable t is: ", type(t)) #the type of variable t is:  <class 'str'>
+print("the type of variable r is: ", type(r)) #the type of variable r is:  <class 'str'>
+print("the type of variable s is: ", type(z)) #the type of variable s is:  <class 'str'>
+
