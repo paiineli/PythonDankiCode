eff2cd50281ef3897e820ebfa1674ce6d20c046d
Lucas Paineli <painelilucas@gmail.com>
2024-07-17 11:23:36 -0300

    Update lesson 5

---
+++ Repository/01VariablesAndDataTypes/03Variables.py
@@ -0,0 +1,24 @@
+x = 5 # x gets the value 5
+y = 3.5
+z = 1+2j
+
+w = "Lucas"
+w = 'Lucas'
+
+# A - z, 0-9 and __
+# first, First and FIRST
+
+h, j, l = 1, 3, 5
+print(h, j, l)
+
+a = b = c = "Python"
+print(a, b, c)
+
+h = w + a 
+print(h)
+
+l = x + j 
+print(l)
+
+l = w + x #TypeError: can only concatenate str (not "int") to str
+print(l)
---
+++ Repository/01VariablesAndDataTypes/04Strings.py
@@ -0,0 +1,34 @@
+print("Hi")
+print('Hi')
+
+w = "Python3 course"
+print(w)
+
+"""comment"""
+
+a = """Hi, 
+this is a python course
+I hope you enjoy it"""
+
+print(a)
+
+#string methods
+
+b = " Hello World "
+print(b)
+print(b.strip())
+
+c = "Hello World"
+print(c)
+print(c.lower())
+
+d = "hello world"
+print(d)
+print(c.upper())
+
+e = "Hello"
+f = "World"
+g = e + f
+print(g)
+g = e + " " + f
+print(g)
