48433e17728f714274ff4109584bc01931429453
Lucas Paineli <painelilucas@gmail.com>
2024-07-17 15:35:57 -0300

    create comparison

---
+++ Repository/02ConditionalAndLogicalStructures/04Comparison.py
@@ -0,0 +1,23 @@
+"""
+comparison operator:
+
+equality ==
+difference !=
+greater than >
+less than <
+greater than or equal to >=
+less than or equal to <=
+
+"""
+
+x = 3
+y = 5
+z = 3.0
+w = 8
+
+print(x == y)
+print(x != z)
+print(x > w)
+print(x < y)
+print(x >= z)
+print(x <= w)
