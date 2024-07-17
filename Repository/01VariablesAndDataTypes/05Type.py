e64aeb442f05749d94dbac2da42fb926d7694bf4
Lucas Paineli <painelilucas@gmail.com>
2024-07-17 11:29:15 -0300

    Update 05Type.py

---
+++ Repository/01VariablesAndDataTypes/05Type.py
@@ -0,0 +1,9 @@
+a = "Hello World"
+b = 5
+c = 3.
+d = 1+2j
+
+print(type(a))  # <class 'str'>
+print(type(b))  # <class 'int'>
+print(type(c))  # <class 'float'>
+print(type(d))  # <class 'complex'>
